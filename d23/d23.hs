bbe4601b419e2aafe4c434817cc21e45ce8fe80c
cyberseihis <mcraftdm@gmail.com>
2022-12-24 01:00:01 +0200

    Day23 full

--- d23/d23.hs
+++ d23/d23.hs
@@ -1,7 +1,9 @@
 import System.IO
 import Data.Functor ((<&>))
 import Santa
-import Data.Maybe (mapMaybe)
+import Data.Maybe (mapMaybe, fromMaybe)
+import Data.List (tails, find, intersect)
+import Debug.Trace (traceShow)
 
 data Elf = Elf Int Int deriving (Eq,Show)
 
@@ -26,9 +28,83 @@ inflate 0 = [-1,0,1]
 inflate x = [x]
 
 expand :: Coord -> [Coord]
-expand (Elf x y) = [Elf i j | x /= 0 || y /= 0, i <- inflate x, j <- inflate y]
+expand (Elf x y) = [Elf i j | i <- inflate x, j <- inflate y, i /= 0 || j /= 0]
 
-part1 = error "a" . compr
+data Face = I | N | S | W | E deriving (Eq,Show)
+
+vidot :: Face -> Coord
+vidot I = Elf 0 0
+vidot N = Elf (-1) 0
+vidot S = Elf 1 0
+vidot W = Elf 0 (-1)
+vidot E = Elf 0 1
+
+fov :: Face -> [Coord]
+fov = expand . vidot
+
+move :: Elf -> Face -> Elf
+move elf face = elf + vidot face
+
+cards = map (take 4) . tails . cycle $ [N,S,W,E]
+
+spot :: [Elf] -> Elf -> Face -> Bool
+spot elves elf = any ((`elem`elves) . (+elf)) . fov
+
+next :: [Elf] -> [Face] -> Elf -> Elf
+next elves faces myself =
+    let near = hood elves myself
+        lonely = not $ spot near myself I
+        nface = fromMaybe I $ find (not.spot near myself) faces
+    in move myself $ if lonely then I else nface
+
+isUniq :: Eq a => [a] -> a -> Bool
+isUniq xs x = (==1) . length . filter (==x) $ xs
+
+hood :: [Elf] -> Elf -> [Elf]
+hood elves elf = elves `intersect` ([elf+x|x<-fov I])
+
+wantToGo :: [Elf] -> [Face] -> [Elf]
+wantToGo elves faces = map (next elves faces) elves
+
+click :: [Elf] -> [Face] -> [Elf]
+click elves faces =
+    let wanna = wantToGo elves faces
+        ok = map (isUniq wanna) wanna
+        ite x y z = if x then y else z
+    in zipWith3 ite ok wanna elves
+
+deck = take 10 cards
+
+bigSquare :: [Elf] -> Int
+bigSquare elves =
+    let j = map (\(Elf x y)->x) elves
+        i = map (\(Elf x y)->y) elves
+        mx = 1 + maximum j - minimum j
+        my = 1 + maximum i - minimum i
+    in mx * my
+
+solve elves = bigSquare elves - length elves
+
+part1 lins =
+    let begin = compr lins
+    in solve $ foldl click begin deck
+
+click2 :: ([Elf],Int) -> [[Face]] -> Int
+click2 _ [] = 0
+click2 (elf,i) (fac:fs) =
+    let nx = traceShow i $ click elf fac
+    in if elf==nx then i else click2 (nx,succ i) fs
+
+solve2 = snd
+
+part2 lins =
+    let begin = compr lins
+    in click2 (begin,1) cards
 
 test1 = wrD part1
 result1 = wrI part1
+
+test2 = wrD part2
+result2 = wrI part2
+
+main = print =<< result2
